1171b7ad915c150b5a1c96feede0a0a0e2d14dad
Ridwan Jauhar Kafabihi <145266609+RidwanJk@users.noreply.github.com>
2024-06-18 22:42:34 +0700

    new file:   Oh! Asmara.txt 	modified:   api.py 	deleted:    beat it.txt 	deleted:    lyrics.txt 	new file:   sway to my beat in cosmos.html

--- api.py
+++ api.py
@@ -18,27 +18,36 @@ def get_song_lyrics(song_title, api_token):
         if song_title.lower() in hit['result']['title'].lower():
             song_info = hit
             break
-
+    
     if song_info:
         song_url = song_info['result']['url']
-        page = requests.get(song_url)
-        html = BeautifulSoup(page.text, "html.parser")
-        lyrics = html.find("div", class_="lyrics").get_text() if html.find("div", class_="lyrics") else "Lyrics not found"
-        return lyrics
+        lyrics_response = requests.get(song_url, headers=headers)
+        return lyrics_response.text
     else:
         return "Lyrics not found"
-
+    
 def SaveLyrics(lyrics, filename):
-    with open(filename, "w") as file:
+    # create file
+    with open(filename, "w", encoding="utf-8") as file:
         file.write(lyrics)
     print(f"Lyrics saved to {filename}")
+    
+def extract_lyrics(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    lyrics_div = soup.find_all('div', {'data-lyrics-container': 'true'})
+    if lyrics_div:
+        return str(lyrics_div)
+    else:
+        return "Lyrics not found in the expected format"
 
 def main():
     song_title = input("Enter the song title: ")
-    api_token = "7BUjs-LqTGZ1MxOcoPoSQh9XQ-OGNnwZ9s1_kaTYoTezILHEKfsax7tduZZBNR4N"    # Replace with your actual Genius API token
+    api_token = "7BUjs-LqTGZ1MxOcoPoSQh9XQ-OGNnwZ9s1_kaTYoTezILHEKfsax7tduZZBNR4N"  # Replace with your actual Genius API token
 
-    lyrics = get_song_lyrics(song_title, api_token)
-    SaveLyrics(lyrics, f"{song_title}.txt")
+    lyrics = get_song_lyrics(song_title, api_token)    
+    extractedLyrics = extract_lyrics(lyrics)
+    SaveLyrics(extractedLyrics, f"{song_title}.txt")
+    
 
 if __name__ == "__main__":
     main()
